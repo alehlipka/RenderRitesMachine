377d1085f8566c45a0944e5b62e645e08cac090e
alehlipka <alehlipka@gmail.com>
2025-06-18 17:08:50 +0300

    base intersect

--- RenderRitesMachine/ECS/Features/Outline/Systems/OutlineRenderSystem.cs
+++ RenderRitesMachine/ECS/Features/Outline/Systems/OutlineRenderSystem.cs
@@ -1,9 +1,12 @@
 using System.Runtime.CompilerServices;
 using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 using RenderRitesMachine.ECS.Features.Mesh.Components;
 using RenderRitesMachine.ECS.Features.Outline.Components;
+using RenderRitesMachine.ECS.Features.PerspectiveCamera.Components;
 using RenderRitesMachine.ECS.Features.Transform.Components;
 using RenderRitesMachine.ECS.Systems;
+using RenderRitesMachine.Utilities;
 
 namespace RenderRitesMachine.ECS.Features.Outline.Systems;
 
@@ -11,20 +14,34 @@ public class OutlineRenderSystem : IRenderSystem
 {
     public void Render(float deltaTime, World world)
     {
-        foreach (ITuple tuple in world.GetComponents(typeof(TransformComponent), typeof(MeshComponent), typeof(OutlineShaderComponent)))
+        MouseState mouse = RenderRites.Machine.Window!.MouseState;
+        
+        foreach (ITuple tuple in world.GetComponents(typeof(TransformComponent), typeof(MeshComponent), typeof(OutlineShaderComponent), typeof(PerspectiveCameraComponent)))
         {
             TransformComponent transform = (TransformComponent)tuple[0]!;
             MeshComponent mesh = (MeshComponent)tuple[1]!;
             OutlineShaderComponent shader = (OutlineShaderComponent)tuple[2]!;
+            PerspectiveCameraComponent camera = (PerspectiveCameraComponent)tuple[3]!;
             
-            GL.Disable(EnableCap.DepthTest);
-            GL.CullFace(TriangleFace.Front);
-            shader.Use();
-            shader.SetMatrix4("model", transform.ModelMatrix);
-            GL.BindVertexArray(mesh.Vao);
-            GL.DrawElements(mesh.PrimitiveType, mesh.Count, mesh.DrawElementsType, mesh.IndicesStoreLocation);
-            GL.CullFace(TriangleFace.Back);
-            GL.Enable(EnableCap.DepthTest);
+            if (mouse.IsButtonDown(MouseButton.Left))
+            {
+                float? hitDistance = Ray
+                    .GetFromScreen(mouse.X, mouse.Y, camera.Position, camera.ProjectionMatrix, camera.ViewMatrix)
+                    .TransformToLocalSpace(transform.ModelMatrix)
+                    .IntersectsAABB(mesh.Minimum, mesh.Maximum);
+
+                if (hitDistance != null)
+                {
+                    GL.Disable(EnableCap.DepthTest);
+                    GL.CullFace(TriangleFace.Front);
+                    shader.Use();
+                    shader.SetMatrix4("model", transform.ModelMatrix);
+                    GL.BindVertexArray(mesh.Vao);
+                    GL.DrawElements(mesh.PrimitiveType, mesh.Count, mesh.DrawElementsType, mesh.IndicesStoreLocation);
+                    GL.CullFace(TriangleFace.Back);
+                    GL.Enable(EnableCap.DepthTest);
+                }
+            }
         }
     }
 }
---
+++ RenderRitesMachine/Utilities/Ray.cs
@@ -0,0 +1,70 @@
+using OpenTK.Mathematics;
+
+namespace RenderRitesMachine.Utilities;
+
+public class Ray(Vector3 origin, Vector3 direction)
+{
+    public Vector3 Origin { get; } = origin;
+    public Vector3 Direction { get; } = direction;
+    
+    public Ray TransformToLocalSpace(Matrix4 modelMatrix)
+    {
+        Matrix4 inverseModel = modelMatrix.Inverted();
+        Vector3 localOrigin = Vector3.TransformPosition(Origin, inverseModel);
+        Vector3 localDirection = Vector3.TransformNormal(Direction, inverseModel);
+        localDirection.Normalize(); // Важно нормализовать после трансформации!
+        return new Ray(localOrigin, localDirection);
+    }
+    
+    public float? IntersectsAABB(Vector3 boxMin, Vector3 boxMax)
+    {
+        // Алгоритм пересечения луча и AABB (Slab Method)
+        float tMin = float.MinValue;
+        float tMax = float.MaxValue;
+
+        for (int i = 0; i < 3; i++) // Проверяем по осям X, Y, Z
+        {
+            if (Math.Abs(Direction[i]) < float.Epsilon) // Луч параллелен оси
+            {
+                if (Origin[i] < boxMin[i] || Origin[i] > boxMax[i])
+                    return null; // Нет пересечения
+            }
+            else
+            {
+                float invDir = 1.0f / Direction[i];
+                float t1 = (boxMin[i] - Origin[i]) * invDir;
+                float t2 = (boxMax[i] - Origin[i]) * invDir;
+
+                if (t1 > t2) (t1, t2) = (t2, t1); // Упорядочиваем t1 и t2
+
+                tMin = Math.Max(tMin, t1);
+                tMax = Math.Min(tMax, t2);
+
+                if (tMin > tMax) return null; // Нет пересечения
+            }
+        }
+
+        // Возвращаем ближайшее расстояние (tMin), если оно положительное
+        return tMin >= 0 ? tMin : null;
+    }
+
+    public static Ray GetFromScreen(float mouseX, float mouseY, Vector3 cameraPosition, Matrix4 projection, Matrix4 view)
+    {
+        Vector2i windowSize = RenderRites.Machine.Window!.Size;
+        
+        // Нормализованные координаты устройства (NDC)
+        float x = (2.0f * mouseX) / windowSize.X - 1.0f;
+        float y = 1.0f - (2.0f * mouseY) / windowSize.Y;
+        Vector3 rayNormalizedDeviceCoords = new(x, y, -1.0f); // Z = -1 (направление вглубь экрана)
+
+        // Преобразование в мировые координаты
+        Vector4 rayClip = new(rayNormalizedDeviceCoords.X, rayNormalizedDeviceCoords.Y, -1.0f, 1.0f);
+        Vector4 rayEye = rayClip * projection.Inverted();
+        rayEye = new Vector4(rayEye.X, rayEye.Y, -1.0f, 0.0f); // Убираем перспективу
+        Vector3 rayWorld = (rayEye * view.Inverted()).Xyz;
+        rayWorld.Normalize();
+
+        // Возвращаем луч: начало (позиция камеры) и направление
+        return new Ray(cameraPosition, rayWorld);
+    }
+}
